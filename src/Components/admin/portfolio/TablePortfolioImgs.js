3fe28dbc30e14a8c76f8d29907f63688c9f6edc2
Michal <mmatynia@gmail.com>
2019-03-30 22:53:32 +0100

    portfolio 2

--- src/Components/admin/portfolio/TablePortfolioImgs.js
+++ src/Components/admin/portfolio/TablePortfolioImgs.js
@@ -28,36 +28,21 @@ class TablePortfolioImgs extends Component {
         const parentID = this.props.entityID;
         let portfolio_imgs = [];
 
-        firebaseDB.ref('portfolio_imgs').orderByChild("parentID").equalTo(parentID).once("value", function(snapshot) {
-            // console.log(snapshot.val());
-            let loop = firebaseLooper(snapshot) ;
-            //portfolio_imgs[portfolio_imgs.length] = firebaseLooper(snapshot) ;
-            portfolio_imgs.push(firebaseLooper(snapshot)) ;
+        firebaseDB.ref('portfolio_imgs').child("parentID").equalTo(parentID).once("value")
+        .then((snapshot) => {
+            const portfolio_imgs = firebaseLooper(snapshot);
 
-
+            this.setState({
+                isloading: false,
+                portfolio_imgs
+            })   
+        })
  
-/*             snapshot.forEach((childSnapshot) => {
-
-                
-
-                let grandChildSnaphot = childSnapshot.val()
-                let grandChildSnaphotKey = childSnapshot.key
-
-            }) */
-
-          });
           
           console.log(typeof(portfolio_imgs));
           console.log(this.state.portfolio_imgs);
 
-          this.setState({
-            isloading: false,
-            portfolio_imgs: portfolio_imgs[0]
-        })   
 
-/*         this.setState({
-            parentID
-        }) */
 
     }
 
